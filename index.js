a374d8911d9cb2326a57166feb66fa5cc3b5476a
gauthier-huyghe <gauthier.huyghe@gmail.com>
2019-07-04 17:20:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.onkeydown = KeyPress;
 function KeyPress(e) {
-  const evtobj = window.event ? window.event : e;
+  var evtobj = window.event ? window.event : e;
   if (evtobj.keyCode === 75 && evtobj.shiftKey && evtobj.ctrlKey && evtobj.altKey && evtobj.metaKey) {
     console.log('YEAAAAHHHHH !!\r\nKorhanola Rulesssss !\r\n\______________\r\nPFFFFFFF !');
     var regex = /\b(\w+(?![^<>]*>))\b/g;
